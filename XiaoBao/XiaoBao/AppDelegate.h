34e33ea17ba6f2cd90ca7906d626dbf6613abd97
chenyalun <iyalun@outlook.com>
2017-02-15 12:26:33 +0800

    初始化项目

---
+++ XiaoBao/XiaoBao/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XiaoBao
+//
+//  Created by 陈亚伦 on 2017/2/15.
+//  Copyright © 2017年 陈亚伦. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
